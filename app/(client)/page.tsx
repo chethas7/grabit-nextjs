a62a1d816746504f3e43f91853c876998c0dec80
chethas7 <chethas7@gmail.com>
2025-06-09 20:13:20 +0530

    imported data to sanity studio

---
+++ app/(client)/page.tsx
@@ -0,0 +1,13 @@
+import Banner from '@/components/Banner'
+import Container from '@/components/Container'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Container className='p-10 bg-shop_banner_blue'>
+      <Banner />
+    </Container>
+  )
+}
+
+export default Home
---
+++ app/(client)/shop/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            SHOP
+        </div>
+    )
+}
+
+export default page
